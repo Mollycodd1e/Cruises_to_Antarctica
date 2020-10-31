1ef963667da5e4af1dc3686445e480e03db332b3
Mollycodd1e <selik7@mail.ru>
2020-10-31 20:20:59 +0300

    Добавление блока шапки

--- source/js/toggleButton.js
+++ source/js/toggleButton.js
@@ -1,25 +1,18 @@
 'use strict';
 var mainNav = document.querySelector('.main-nav');
 var mainNavToggle = document.querySelector('.main-nav__toggle');
+var logo = document.querySelector('.main-nav__logo-image');
 
 mainNav.classList.remove('main-nav--nojs');
-
-window.addEventListener("resize", function() {
-  if (window.matchMedia("(min-width: 767px)").matches) {
-    mainNav.classList.add('main-nav--closed');
-    mainNav.classList.remove('main-nav--opened');
-  } else {
-    mainNav.classList.remove('main-nav--closed');
-    mainNav.classList.add('main-nav--opened');
-  }
-}),
 
 mainNavToggle.addEventListener('click', function () {
   if (mainNav.classList.contains('main-nav--closed')) {
     mainNav.classList.remove('main-nav--closed');
     mainNav.classList.add('main-nav--opened');
+    logo.src = "img/logo-black.png";
   } else {
     mainNav.classList.add('main-nav--closed');
     mainNav.classList.remove('main-nav--opened');
+    logo.src = "img/logo-desktop.png";
   }
 });
