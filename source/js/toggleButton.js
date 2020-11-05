8bdc55df0f9b5877408d6542b976fcc13e37b679
Mollycodd1e <selik7@mail.ru>
2020-11-06 00:05:37 +0300

    Доисправление замечаний

--- source/js/toggleButton.js
+++ source/js/toggleButton.js
@@ -5,35 +5,16 @@
 
   if (document.querySelector('.main-nav')) {
     var mainNavToggle = document.querySelector('.main-nav__toggle');
-    var logo = document.querySelector('.main-nav__logo img');
-    var whiteLogoDesktop = 'img/logo-desktop.png';
-    var whiteLogoDesktopRetina = 'img/logo-desktop@2x.png';
-    var blackLogo = 'img/logo-black.png';
-    var blackLogoRetina = 'img/logo-black@2x.png';
 
     mainNav.classList.remove('main-nav--nojs');
-
-    var changeOnWhiteLogo = function () {
-      logo.src = whiteLogoDesktop;
-      logo.srcset = whiteLogoDesktopRetina;
-    };
-
-    var changeOnBlackLogo = function () {
-      logo.src = blackLogo;
-      logo.srcset = blackLogoRetina;
-    };
-
-    changeOnWhiteLogo();
 
     mainNavToggle.addEventListener('click', function () {
       if (mainNav.classList.contains('main-nav--closed')) {
         mainNav.classList.remove('main-nav--closed');
         mainNav.classList.add('main-nav--opened');
-        changeOnBlackLogo();
       } else {
         mainNav.classList.add('main-nav--closed');
         mainNav.classList.remove('main-nav--opened');
-        changeOnWhiteLogo();
       }
     });
 
@@ -41,7 +22,6 @@
       if (window.innerWidth > 767 && mainNav.classList.contains('main-nav--opened') === true) {
         mainNav.classList.add('main-nav--closed');
         mainNav.classList.remove('main-nav--opened');
-        changeOnWhiteLogo();
       }
     });
   }
