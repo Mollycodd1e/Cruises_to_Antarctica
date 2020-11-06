b2696b5b228162ba555f3432bf0db2fd994bdf50
Mollycodd1e <selik7@mail.ru>
2020-11-06 12:02:25 +0300

    исправление ошибок

--- source/js/formValidity.js
+++ source/js/formValidity.js
@@ -1,9 +1,9 @@
 'use strict';
 
 (function () {
-  var form = document.querySelector('.booking__form');
+  var form = document.querySelector('form');
 
-  if (document.querySelector('.booking__form')) {
+  if (document.querySelector('form')) {
 
     var formName = form.querySelector('.booking__name-wrapper input');
     var formNameErrorMessage = 'Введите ваше имя';
