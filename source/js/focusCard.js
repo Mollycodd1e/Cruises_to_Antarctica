10efbf0d8b5c7e9b653600ed84f3c45f398d344b
Mollycodd1e <selik7@mail.ru>
2020-11-11 17:21:21 +0300

    исправление фокуса

--- source/js/focusCard.js
+++ source/js/focusCard.js
@@ -7,16 +7,24 @@
 
     var allCards = cardsList.querySelectorAll('li');
     var allButtons = cardsList.querySelectorAll('a');
+    var allDescription = cardsList.querySelectorAll('.description');
 
-    var getFocus = function (card) {
+    var getFocus = function (card, button, description) {
       card.addEventListener('focus', function () {
-        allButtons[i].focus();
-        card[i].style = 'opacity: 1';
+        card.setAttribute('tabindex', '-1');
+        description.style = 'opacity: 1';
+        button.focus();
+      });
+
+      button.addEventListener('blur', function () {
+        card.setAttribute('tabindex', '0');
+        description.style = 'opacity: 0';
+        button.blur();
       });
     };
 
     for (var i = 0; i < allCards.length; i++) {
-      getFocus(allCards[i]);
+      getFocus(allCards[i], allButtons[i], allDescription[i]);
     }
   }
 })();
